ee69f45c7bfcfea8c4c7e170438fd35be0001649
Felipe Raposo <felipe_ayslan@yahoo.com.br>
2023-10-18 16:49:05 -0300

    Testes realizados com sucesso

---
+++ NewTalents/NewTalents/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace NewTalents
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //Calculadora calc = new Calculadora("18/10/2023");
+
+            //string opcao;
+            //bool exibirMenu = true;
+
+            //while (exibirMenu)
+            //{
+            //    Console.Clear();
+            //    Console.WriteLine("Digite a sua opção: ");
+            //    Console.WriteLine("1 - Somar");
+            //    Console.WriteLine("2 - Subtrair");
+            //    Console.WriteLine("3 - Multiplica");
+            //    Console.WriteLine("4 - Dividir");
+            //    Console.WriteLine("5 - Listar histórico");
+
+            //    opcao = Console.ReadLine();
+
+            //    switch (opcao)
+            //    {
+            //        case "1":
+            //            //Console.WriteLine("Cadastro cliente");
+            //            calc.Somar();
+            //            break;
+
+            //        case "2":
+            //            //Console.WriteLine("Busca cliente");
+            //            calc.subtrair();
+            //            break;
+
+            //        case "3":
+            //            //Console.WriteLine("Apagar cliente");
+            //            calc.multiplicar();
+            //            break;
+
+            //        case "4":
+            //            calc.dividir();
+            //            break;
+
+            //        case "5":
+            //            calc.historico();
+            //            break;
+
+            //        default:
+            //            Console.WriteLine("Opção inválida");
+            //            break;
+            //    }
+
+            //}
+        }
+    }
+}
